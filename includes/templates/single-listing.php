589f62dc7910eea57aa0f9119ae9708c165b67a2
michaellautman <michaellautman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 02:12:41 +0000

    Tagging 1.4.8
    git-svn-id: https://plugins.svn.wordpress.org/simple-directory/tags/1.4.8@1193798 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/single-listing.php
@@ -0,0 +1,143 @@
+<?php /**
+* The template for displaying single listings
+*
+**/
+
+get_header(); ?>
+<div id="primary" class="content-area"><!--PRIMARY-->
+		<div id="content" class="site-content" role="main"><!--CONTENT-->
+		<?php while ( have_posts() ) : the_post(); ?>
+<div class="row"><!--START CONTENT WRAP ROW-->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> <!--ARTICLE-->
+	<?php $dir_meta = get_post_meta($post->ID);				?>
+		<div class="small-12 columns"><!--START CONTENT WRAP COLUMN-->
+		<?php if(isset($dir_meta['listing_type'][0]) && $dir_meta['listing_type'][0] == 'organization') {
+		echo '<div itemscope itemtype="http://schema.org/LocalBusiness">';
+		} ?>
+		<?php if(isset($dir_meta['listing_type'][0]) && $dir_meta['listing_type'][0] == 'individual') {
+			echo '<div itemscope itemtype ="http://schema.org/Person">';
+		} ?>
+		<!--START LOCAL BUSINESS INFO-->
+			<div class="row"><!--START HEADER AREA-->
+			<div class="medium-8  columns"><!--TITLE-LOGO-CONTACT-->
+			<div class="row"><!--TITLE-LOGO ROW--> 
+		<div class="medium-8 columns">
+		
+		<?php the_title( '<span itemprop="name"><h1 class="single-listing-title">', '</h1><span>' ); ?>
+		<?php if(isset($dir_meta['listing_type'][0]) && $dir_meta['listing_type'][0] == 'individual') {
+			if(isset($dir_meta['listing_ind_title'][0])){
+			echo'<span itemprop="jobTitle">' . $dir_meta['listing_ind_title'][0] .'</span><br/>';
+			}
+			if(isset($dir_meta['listing_ind_org'][0])) { 
+			echo '<span itemprop="worksFor"><strong>' .  $dir_meta['listing_ind_org'][0] . '</strong></span>' ;
+			}
+		}?>
+			</div>
+				<div class="medium-4 columns single-logo" >
+							
+				<?php echo get_the_post_thumbnail(); ?>
+					</div>
+					</div><!--END TITLE-LOGO ROW-->
+			<div class="row"><!--ADDRESS ROW-->
+			<div class="small-12 columns">
+			<?php simple_dir_address_output();?>
+			</div>
+			</div><!--END ADRESS ROW-->
+			<div class="row"><!--START LISTING-PHONE-DIRECTIONS-->
+			<div class="medium-4 small-12 columns">
+			<?php if($dir_meta['listing_phone_1'][0] !='') {?>	
+			<i class="fi-telephone single-listing-icon"></i> <a class="single-listing-meta" href="tel:<?php echo $dir_meta['listing_phone_1'][0];?>">
+						<?php echo $dir_meta['listing_phone_1'][0];?></a>
+				<?php }?>
+			</div>
+			<div class="medium-4 small-12 columns">
+				<?php if ($dir_meta['listing_website_url'][0] !='') {?>
+			<i class="fi-laptop single-listing-icon"></i> <a class="single-listing-meta" href="http://<?php echo $dir_meta['listing_website_url'][0];?>" target="_blank" rel="nofollow"><?php _e('Website','simple-dir');?></a>
+				<?php }?>
+			</div>
+			<div class="medium-4 small-12 columns">
+				
+				<?php if($dir_meta['listing_street_address'][0] !='') {?>
+				<?php   $mapStreet = get_post_meta($post->ID,'listing_street_address', true);
+    	 $mapStreet = preg_replace('/\s+/', '+', $mapStreet);
+				  $_SESSION['mapStreet'] = $mapStreet;
+				$mapCity = get_post_meta($post->ID,'listing_city',true);
+				$mapCity = preg_replace('/\s+/','+',$mapCity);
+					$_SESSION['mapCity'] = $mapCity;
+				$mapPostcode = get_post_meta($post->ID,'listing_postalcode',true);
+				$mapPostcode = preg_replace('/\s+/','+',$mapPostcode);
+				$_SESSION['mapPostcode'] = $mapPostcode;
+				?>
+				<i class="fi-marker single-listing-icon"></i> <a class="single-listing-meta" href="https://www.google.com/maps/dir/Current+Location/<?php echo $_SESSION['mapStreet']; ?>+<?php echo $_SESSION['mapCity']; ?>+<?php echo $_SESSION['mapPostcode']; ?>" target="_blank"
+																 ><?php _e('Get Directions','simple-dir');?></a> <?php }?>
+			</div>
+			</div><!--END PHONE-DIRECTIONS-WEBSITE ROW-->
+				
+			<div class="row"><!--START SOCIAL ROW-->
+			</div><!--END SOCIAL ROW-->
+				
+			<div class="small-12 columns hide-for-small" id="single-social-block">
+			<?php if($dir_meta['listing_status'][0] =='premium') {
+				simple_dir_social_block(); }?>			
+			</div>
+		</div><!--END TITLE-LOGO-CONTACT-->
+		<div class="medium-4 columns hide-for-small"><!--MAP-->
+		
+			<?php if ($dir_meta['listing_street_address'][0] !='') {?>
+			<iframe src="https://www.google.com/maps/embed/v1/search?key=AIzaSyAMFTG0wknrUxibhscMgOXWtcLCUi1tgmM&q=<?php echo $_SESSION['mapStreet']; ?>+<?php echo $_SESSION['mapCity']; ?>+<?php echo $_SESSION['mapPostcode']; ?>" 
+			width="350" height="150" frameborder="0" style="border:0"></iframe> <?php }?>
+			</div><!-- END MAP-->
+		</div><!--END HEADER AREA-->
+<div class="row row-padded"><!--CONTENT ROW-->
+			<div class="small-12 columns"><!--LISTING DESCRIPTION-->
+			<span itemprop="description"><?php the_content(); ?></span>
+	
+		
+		<!--<div class="gallery js-flickity" data-flickity-options='{ "cellAlign": "left", "contain": true, "pageDots":false,"setGallerySize":false}'> -->
+					
+				<?php	//$image_ids = get_post_meta($post->ID, 'simple_dir_slider_upload'); 
+//foreach ($image_ids as $image)
+//{
+  //$myupload = wp_get_attachment_image_src($image, 'medium');				
+//echo '<div class="gallery-cell"><img src="';
+//echo $myupload[0];
+//echo '"/></div>';
+ 
+   // Displays all data
+//}
+?><!--</div> -->
+			</div><!--END LISTING DESCRIPTION-->
+	
+
+ 
+			</div><!--END CONTENT ROW-->
+		</div> <!--END LOCAL BUSINESS INFO-->
+		
+		
+		
+</div><!--END CONTENT WRAP COLUMN-->
+</article><!--END ARTICLE-->
+
+	<?php endwhile; ?>
+
+
+	
+<?php 
+$directory_settings = get_option('simple_directory_settings');
+	$show_sidebar = $directory_settings['single_listing_show_sidebar'];
+	if ($show_sidebar == 'yes')
+	echo get_sidebar();
+	?>
+
+</div> <!--END CONTENT WRAP ROW-->
+</div><!--END CONTENT-->
+</div><!--END PRIMARY-->
+	<?php $dir_settings = get_option('simple_directory_settings');
+$comments = isset($dir_settings['show_comments']);
+if ($comments == 'yes'){?>
+		<div class="row">
+<div class="small-12 columns">
+<?php comments_template(); ?>
+</div>
+</div> <?php }?>
+<?php get_footer();?>
--- parts/readme.txt
+++ parts/readme.txt
@@ -91,8 +91,10 @@ METHOD 3 - FTP
 Got questions?  See the [documentation](http://contexta.ca/simple-directory/simple-directory-documentation/ "Simple Directory Documentation").
 
 == Changelog ==
-= 1.4.8 =
-Cleaned up templates files.
+= 1.4.7 =
+Added Relevanssi to list of recommended plugins.
+= 1.4.6 =
+Fixed search shortcode bug.
 = 1.4.4 =
 Fixed problems with ability to disable Foundation CSS.
 = 1.4.3 =
@@ -124,6 +126,8 @@ Updates to archive pages.  Premium listings are now shown above basic listings.
 *Added settings page.
 
 == Upgrade Notice ==
+= 1.4.6 =
+Fixed bug with search shortcode.
 = 1.4.2 =
 Important upgrade.  Fixes errors on plugin activation.
 = 1.3.3 =
