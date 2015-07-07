383f4c7da9c783ae481cc456c004263b2bfdbe3c
michaellautman <michaellautman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 02:10:46 +0000

    1.4.8
    git-svn-id: https://plugins.svn.wordpress.org/simple-directory/trunk@1193797 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/parts/meta-boxes/listing-metabox.php
@@ -0,0 +1,202 @@
+<?php
+/*
+Title:Listing Details
+Post Type: listing
+Priority: high
+*/
+piklist ('field', array(
+	'type' => 'radio'
+	,'field' => 'listing_type'
+	,'label' =>__('Type')
+	,'value' =>'organization'
+	,'attributes' => array(
+		'class' =>'text'
+	)
+	,'choices' => array(
+	'individual' => __('Individual')
+	,'organization' => __('Organization')
+	)
+	));
+	
+piklist ('field', array(
+		'type' => 'text'
+		,'field' =>'listing_ind_org'
+		,'label' => __('Organization')
+		,'attributes' => array(
+			'class' => 'text'
+			)
+		,'conditions' => array(
+		array(
+			'field' => 'listing_type'
+			,'value' =>'individual'
+			))
+		,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+			));
+piklist ('field' ,array(
+		'type' =>'text'
+		,'field' =>'listing_ind_title'
+		,'label' =>__('Title')
+		,'attributes'=>array(
+			'class' =>'text'
+			)
+		,'conditions' => array(
+			array(
+				'field' => 'listing_type'
+				,'value' =>'individual'
+				))
+		,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+			));
+piklist ('field', array(
+	'type' => 'text',
+	'field' => 'listing_email',
+	'label' => 'Email Address',
+	'attributes' => array(
+		'class' => 'text')
+	,'sanitize' => array(
+      array(
+        'type' => 'email'
+      )
+    )
+));
+
+piklist ('field', array(
+	'type' => 'text',
+	'field' => 'listing_phone_1',
+	'label' => 'Telephone Number',
+	'description' => 'Primary Telephone Number',
+	'attributes' => array(
+		'class' =>'text'
+	)
+	,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+));
+
+piklist ('field', array(
+	'type' => 'text',
+	'field' => 'listing_phone_2',
+	'label' => 'Telephone Number',
+	'description' => 'Other Telephone Number',
+	'attributes' => array(
+		'class' =>'text'
+	)
+	,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+)); 
+
+piklist ('field', array(
+	'type' => 'text',
+	'field' => 'listing_fax',
+	'label' => 'Fax Number',
+	'description' => '',
+	'attributes' => array(
+		'class' =>'text'
+	)
+	,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+));
+piklist ('field', array(
+	'type' => 'text',
+	'field' => 'listing_website_url',
+	'label' => 'Website URL',
+	'description' =>'The address of your website',
+	'attributes' => array(
+		'class' =>'text')
+		,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+));
+
+
+
+piklist ('field', array(
+	'type'=> 'text',
+	'field'=> 'listing_street_address',
+	'label'=> 'Street Address',
+	'description' => 'Street Address',
+	'attributes' => array(
+		'class' => 'text')
+		,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+));
+
+piklist ('field', array(
+	'type'=> 'text',
+	'field'=> 'listing_city',
+	'label'=> 'City',
+	'description' => 'City',
+	'attributes' => array(
+		'class' => 'text')
+		,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+));
+
+piklist ('field', array(
+	'type'=> 'text',
+	'field'=> 'listing_state',
+	'label'=> 'State/Province',
+	'description' => '',
+	'attributes' => array(
+		'class' => 'text')
+		,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+));
+
+piklist ('field', array(
+	'type'=> 'text',
+	'field'=> 'listing_country',
+	'label'=> 'Country',
+	'description' => '',
+	'attributes' => array(
+		'class' => 'text')
+		,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+));
+
+piklist ('field', array(
+	'type'=> 'text',
+	'field'=> 'listing_postalcode',
+	'label'=> 'ZIP/Postal Code',
+	'description' => '',
+	'attributes' => array(
+		'class' => 'text')
+		,'sanitize' => array(
+      array(
+        'type' => 'text_field'
+      )
+    )
+));
+
+
+
+		
---
+++ parts/includes/settings-output.php
@@ -0,0 +1,23 @@
+<?php
+//Inject Custom CSS
+if ('listing' === get_post_type()){
+add_action( 'wp_head', 'simple_directory_custom_styles' );
+}
+function simple_directory_custom_styles() { 
+
+  echo '<style type="text/css" id="sdir-custom-css">' . "\n";
+ $directory_settings = get_option('simple_directory_settings');
+	$custom_css = $directory_settings['listing_custom_css'];
+	echo $custom_css;
+  echo '</style>' . "\n";
+
+}
+//Footer Credit Link
+function simple_directory_credit_link(){
+	echo '<p>Simple Directory Plugin by <a href="http://mywestisland.info" target="_blank" title="West Island Business Directory">MyWestIsland.INFO: The West Island Business Directory</a>.</p>';
+}
+$directory_settings = get_option('simple_directory_settings');
+$credit_link = $directory_settings['show_credit_link'];
+if ($credit_link =='yes'){
+	add_action('wp_footer','simple_directory_credit_link');
+}
